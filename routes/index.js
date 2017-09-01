6c410f9b8c1f904dd613391616ecf00c07fb529b
dec0mpiled <liimbix@gmail.com>
2017-09-01 11:48:37 +0000

    final

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var twilio = require('twilio');
+var Nexmo = require('nexmo');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,14 +9,22 @@ router.get('/', function(req, res, next) {
 
 router.get('/panic/:person?', function(rec, res, next) {
     
-var client = new twilio('AC89a634a3a642402e68f4b5a7fddcf1ba', '7d0730073645400f7dc9e58551db20b4');
- 
-// Send the text message.
-client.messages.create({
-  to: '2697693161',
-  from: '2694210175',
-  body: 'PanicButton has been activated!'
+var nexmo = new Nexmo({
+  apiKey: '534f15ef',
+  apiSecret: '5e701ff1d463f629'
 });
+
+nexmo.message.sendSms(
+  '12015946976', '12697693161', 'A PanicButton has been pressed!',
+    (err, responseData) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.dir(responseData);
+      }
+    }
+ );
+ 
 
 res.redirect("/");
 
