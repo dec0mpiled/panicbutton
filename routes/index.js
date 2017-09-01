83494a2e5e8c543c1a842286f62fc8ec4e9e7586
dec0mpiled <liimbix@gmail.com>
2017-09-01 15:21:41 +0000

    final

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'PanicButton' });
 });
 
+router.get('/bell', function(req, res, next) {
+  res.render('doorbell', { title: 'Doorbell' });
+});
+
+/* Panicbutton */
+
 router.get('/panic/:person?', function(rec, res, next) {
     
 var nexmo = new Nexmo({
@@ -27,6 +33,31 @@ nexmo.message.sendSms(
  
 
 res.redirect("/");
+
+});
+
+/* ring bell */
+
+router.get('/ring', function(rec, res, next) {
+    
+var nexmo = new Nexmo({
+  apiKey: '534f15ef',
+  apiSecret: '5e701ff1d463f629'
+});
+
+nexmo.message.sendSms(
+  '12015946976', '12697693161', 'Someone would like to enter your room.',
+    (err, responseData) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.dir(responseData);
+      }
+    }
+ );
+ 
+
+res.redirect("/bell");
 
 });
 
